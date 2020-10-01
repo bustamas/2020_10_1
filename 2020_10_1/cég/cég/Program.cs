6312a202e42a84bba2dbf30511ddc804c93beab5
bustamas <43034979+bustamas@users.noreply.github.com>
2020-10-01 15:28:17 +0200

    Add files via upload

---
+++ 2020_10_1/cég/cég/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cég
+{
+    class Program
+    {
+        struct cég
+        {
+            public string ceg_nev;
+            public double nap_1;
+            public double nap_2;
+            public double nap_3;
+            public double ize;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Hány cég van?\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            cég[] brrr = new cég[n];
+            Console.WriteLine("Kérem a cégek napi összegét\n");
+            for (int i = 0; i < brrr.Length; i++)
+            {
+                Console.Write("A cég neve: \n");
+                brrr[i].ceg_nev = Console.ReadLine();
+                Console.Write("\nElső napi bevétel:");
+                brrr[i].nap_1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nMásodik napi bevétel:");
+                brrr[i].nap_2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nHarmadik napi bevétel:");
+                brrr[i].nap_3 = Convert.ToDouble(Console.ReadLine());
+                brrr[i].ize = brrr[i].nap_3-brrr[i].nap_1;
+                Console.WriteLine("{0} volt a nyereség", brrr[i].ize);
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
---
+++ 2020_10_1/éretségi/éretségi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace éretségi
+{
+    class Program
+    {
+        struct éretségi
+        {
+            public string nev;
+            public double matek;
+            public double tori;
+            public double nyelv;
+            public double szakmai;
+            public double magyar;
+            public double osz_atlag;
+            public double tan_atlag;
+            /* legjobb legroszabb és 4,5 felet*/
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Hány tanuló éretségizet?\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            éretségi[] brrr = new éretségi[n];
+            Console.WriteLine("Kérem a tanulók \n");
+            for (int i = 0; i < brrr.Length; i++)
+            {
+                Console.Write("A tanuló neve: \n");
+                brrr[i].nev = Console.ReadLine();
+                Console.Write("\nMatek jegye:");
+                brrr[i].matek= Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nTöri jegye:");
+                brrr[i].tori = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nNyelv jegye:");
+                brrr[i].nyelv = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nSzakmai jegye:");
+                brrr[i].szakmai = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nMagyar jegye:");
+                brrr[i].magyar = Convert.ToDouble(Console.ReadLine());
+                brrr[i].osz_atlag=
+            }
+        }
+    }
+}
